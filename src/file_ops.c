19b20313791333e59576cafbd1d2037508dc20d3
CoolerVoid <coolerlair@gmail.com>
2021-04-18 21:48:26 -0300

    update to prevent toctou

--- src/file_ops.c
+++ src/file_ops.c
@@ -5,35 +5,45 @@
 char 
 *read_lines (char * NameFile)
 {
-	FILE * arq;
+	char *buffer = NULL;
+	int fd = open(NameFile, O_RDONLY | O_EXCL );
+	FILE *arq=NULL;
 
-	arq = fopen(NameFile, "r");
-
-	if( arq == NULL )
+	if (-1 != fd) 
 	{
-		DEBUG("error in filename %s  to open() file:  %s",NameFile,strerror(errno));		
-		exit(0);
-	}
+		arq = fdopen(fd, "rx");
 
-	fseek(arq, 0, SEEK_END);
-    	long length = ftell(arq);
-    	fseek(arq, 0, SEEK_SET);
-    	char *buffer = xmalloc(sizeof(char)*(length + 1));
-    	buffer[length] = '\0';
+		if( arq == NULL )
+		{
+			DEBUG("error in filename %s  to open() file:  %s",NameFile,strerror(errno));		
+			exit(0);
+		}
 	
-    	if (fread(buffer, sizeof(char), length, arq)<=0)
-	{
-		DEBUG("Config error Empty rule file %s",NameFile);
-		exit(0);
-	}
+		fseek(arq, 0, SEEK_END);
+    		long length = ftell(arq);
+    		fseek(arq, 0, SEEK_SET);
+    		buffer = xmalloc(sizeof(char)*(length + 1));
+    		buffer[length] = '\0';
+		
+    		if (fread(buffer, sizeof(char), length, arq)<=0)
+		{
+			DEBUG("Config error Empty rule file %s",NameFile);
+			exit(0);
+		}
  
-	if (fclose(arq) == EOF)
-	{
-		DEBUG("Error in close() file %s",NameFile);
-		exit(0);
+		if (fclose(arq) == EOF)
+		{
+			DEBUG("Error in close() file %s",NameFile);
+			exit(0);
+		}
+	} else {
+		if (close(fd) == -1) 
+		{
+			
+			DEBUG("error in filename %s  to open() file:  %s",NameFile,strerror(errno));		
+			exit(0);
+		}
 	}
-
-	arq = NULL;
 
 	return buffer;
 }
@@ -44,7 +54,7 @@ log_make (char *mode, char *addr_ip, char *data, size_t len_data)
 	size_t len_log=len_data+strlen(addr_ip)+(25*sizeof(char)+strlen(mode));
 	char *log_in=xmalloc(len_log);
 
-	burn_mem(log_in,0, len_log-1); // preserve last byte of canary here, need or can cause crashes
+	burn_mem(log_in,0, len_log-1); // preserve last byte of canary here, need or can cause crashes, warning have a hardening in make file ;-) 
 	snprintf(log_in,len_log,"Mode: %s\nIP addr: %s\nData: %s\n", mode, addr_ip, data);
 	write_log(log_in);
 	free(log_in);
@@ -55,27 +65,39 @@ log_make (char *mode, char *addr_ip, char *data, size_t len_data)
 void 
 write_log (char *str)
 {
-	FILE *arq;
- 
-	arq = fopen(param.logfile,"a"); 
+	int fd = open(param.logfile, O_CREAT | O_WRONLY | O_APPEND, 0760 ); 
+	FILE *arq=NULL;
 
-	if (arq == NULL) 
+	
+	if (-1 != fd) 
 	{
-//		fclose(arq);
-		DEBUG("error in WriteFile() %s",param.logfile);
-	        exit(0);
+
+		arq = fdopen(fd, "ax");
+
+		if (arq == NULL) 
+		{
+			DEBUG("error in filename %s  to open() file:  %s",param.logfile,strerror(errno));		
+		        exit(0);
+		}
+
+		fprintf(arq,"%s\n",str); 
+
+		if (fclose(arq) == EOF)
+		{
+			DEBUG("error in Write() file %s",param.logfile);
+			exit(0);
+		}
+	} else {
+
+		if (close(fd) == -1) 
+		{
+			
+			DEBUG("error in filename %s  to open() file:  %s",param.logfile,strerror(errno));		
+			exit(0);
+		}
 	}
 
-	fprintf(arq,"%s\n",str); 
-
-	if (fclose(arq) == EOF)
-	{
-		DEBUG("error in Write() file %s",param.logfile);
-		exit(0);
-	}
-
-	arq=NULL;
- 
+	arq=NULL; 
 
 }
 
--- src/headers/file_ops.h
+++ src/headers/file_ops.h
@@ -5,6 +5,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include "utils.h"
 #include "opt_extract.h"
 #include "mem_ops.h"
